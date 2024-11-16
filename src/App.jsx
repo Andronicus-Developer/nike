c349a2bcae156553f5598df21bf4647d82ba93ee
Andronicus-Developer <andronicusjohnson475@gmail.com>
2024-11-16 00:23:54 +0000

    all

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Hero, PopularProducts, QualitySuper, Services, SpecialOffer, CustomerReviews, Subscribe, Footer} from "./sections";
+import Navbar from "./components/Navbar"
+
+const App = () => (
+  <main className="relative">
+    <Navbar />
+    <section className="xl:padding-l xl:padding-r wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <QualitySuper />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding=x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+)
+
+export default App;
